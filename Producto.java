2e4ae8c42aa3ed0b71f6ecbd39d0425a12071c54
isa <maris@gmail.com>
2024-08-04 15:39:54 -0500

    Mi primer commit

---
+++ Producto.java
@@ -0,0 +1,16 @@
+public class Producto {
+   int edad =15;
+   String nombre = "maria";
+   Double precio = 15.5;
+   boolean valor = true;
+   
+    public static void main(String[] args) {
+        Producto x = new Producto();
+        System.out.println("Edad: " + x.edad);
+        System.out.println("nombre: " + x.nombre);
+        System.out.println("Precio: " + x.precio);
+        System.out.println("Valor: " + x.valor);
+    }
+    
+    
+}
